5fb71404e2fdcefcdd18aad7feb985000000993c
Álvaro Troya <alvaro.troya@googlemail.com>
2024-02-03 06:06:33 +0100

    Use a match statement

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -34,57 +34,53 @@ impl GildedRose {
 
     pub fn update_quality(&mut self) {
         for item in self.items.iter_mut() {
-            if item.name == "Sulfuras, Hand of Ragnaros" {
-                continue;
-            }
-
-            if item.name == "Aged Brie" {
-                if item.quality < 50 {
-                    item.quality += 1;
-                }
-
-                item.sell_in -= 1;
-
-                if item.sell_in < 0 {
+            match &item.name[..] {
+                "Sulfuras, Hand of Ragnaros" => {}
+                "Aged Brie" => {
                     if item.quality < 50 {
                         item.quality += 1;
                     }
-                }
-                continue;
-            }
 
-            if item.name == "Backstage passes to a TAFKAL80ETC concert" {
-                if item.quality < 50 {
-                    item.quality += 1;
+                    item.sell_in -= 1;
+
+                    if item.sell_in < 0 {
+                        if item.quality < 50 {
+                            item.quality += 1;
+                        }
+                    }
                 }
-                if item.sell_in < 11 {
+                "Backstage passes to a TAFKAL80ETC concert" => {
                     if item.quality < 50 {
                         item.quality += 1;
                     }
-                }
-                if item.sell_in < 6 {
-                    if item.quality < 50 {
-                        item.quality += 1;
+                    if item.sell_in < 11 {
+                        if item.quality < 50 {
+                            item.quality += 1;
+                        }
+                    }
+                    if item.sell_in < 6 {
+                        if item.quality < 50 {
+                            item.quality += 1;
+                        }
+                    }
+
+                    item.sell_in -= 1;
+
+                    if item.sell_in < 0 {
+                        item.quality = 0;
                     }
                 }
+                _ => {
+                    if item.quality > 0 {
+                        item.quality -= 1;
+                    }
 
-                item.sell_in -= 1;
+                    item.sell_in -= 1;
 
-                if item.sell_in < 0 {
-                    item.quality = 0;
+                    if item.sell_in < 0 {
+                        item.quality -= 1;
+                    }
                 }
-                continue;
-            }
-
-
-            if item.quality > 0 {
-                item.quality -= 1;
-            }
-
-            item.sell_in -= 1;
-
-            if item.sell_in < 0 {
-                item.quality -= 1;
             }
         }
     }
