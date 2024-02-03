2ed254eccbbacbff5d026a517740de6b35304e05
Álvaro Troya <alvaro.troya@googlemail.com>
2024-02-03 06:16:36 +0100

    Extract methods for updating each item

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -23,6 +23,54 @@ impl Display for Item {
     }
 }
 
+pub fn update_aged_brie(item: &mut Item) {
+    if item.quality < 50 {
+        item.quality += 1;
+    }
+
+    item.sell_in -= 1;
+
+    if item.sell_in < 0 {
+        if item.quality < 50 {
+            item.quality += 1;
+        }
+    }
+}
+
+pub fn update_backstage_passes(item: &mut Item) {
+    if item.quality < 50 {
+        item.quality += 1;
+    }
+    if item.sell_in < 11 {
+        if item.quality < 50 {
+            item.quality += 1;
+        }
+    }
+    if item.sell_in < 6 {
+        if item.quality < 50 {
+            item.quality += 1;
+        }
+    }
+
+    item.sell_in -= 1;
+
+    if item.sell_in < 0 {
+        item.quality = 0;
+    }
+}
+
+pub fn update_regular_item(item: &mut Item) {
+    if item.quality > 0 {
+        item.quality -= 1;
+    }
+
+    item.sell_in -= 1;
+
+    if item.sell_in < 0 {
+        item.quality -= 1;
+    }
+}
+
 pub struct GildedRose {
     pub items: Vec<Item>,
 }
@@ -36,51 +84,9 @@ impl GildedRose {
         for item in self.items.iter_mut() {
             match &item.name[..] {
                 "Sulfuras, Hand of Ragnaros" => {}
-                "Aged Brie" => {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
-
-                    item.sell_in -= 1;
-
-                    if item.sell_in < 0 {
-                        if item.quality < 50 {
-                            item.quality += 1;
-                        }
-                    }
-                }
-                "Backstage passes to a TAFKAL80ETC concert" => {
-                    if item.quality < 50 {
-                        item.quality += 1;
-                    }
-                    if item.sell_in < 11 {
-                        if item.quality < 50 {
-                            item.quality += 1;
-                        }
-                    }
-                    if item.sell_in < 6 {
-                        if item.quality < 50 {
-                            item.quality += 1;
-                        }
-                    }
-
-                    item.sell_in -= 1;
-
-                    if item.sell_in < 0 {
-                        item.quality = 0;
-                    }
-                }
-                _ => {
-                    if item.quality > 0 {
-                        item.quality -= 1;
-                    }
-
-                    item.sell_in -= 1;
-
-                    if item.sell_in < 0 {
-                        item.quality -= 1;
-                    }
-                }
+                "Aged Brie" => update_aged_brie(item),
+                "Backstage passes to a TAFKAL80ETC concert" => update_backstage_passes(item),
+                _ => update_regular_item(item),
             }
         }
     }
