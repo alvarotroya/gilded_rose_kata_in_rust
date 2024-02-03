d8c9dbfbc04fcdf6016e6d1cd75b5f59d0669341
Álvaro Troya <alvaro.troya@googlemail.com>
2024-02-03 07:01:38 +0100

    Refactor methods for each item

--- rust/src/gildedrose.rs
+++ rust/src/gildedrose.rs
@@ -1,3 +1,4 @@
+use core::cmp::{max, min};
 use std::fmt::{self, Display};
 
 #[derive(PartialEq, Debug)]
@@ -23,52 +24,41 @@ impl Display for Item {
     }
 }
 
-pub fn update_aged_brie(item: &mut Item) {
-    if item.quality < 50 {
-        item.quality += 1;
+pub fn update_regular_item(item: &mut Item) {
+    if item.sell_in > 0 {
+        item.quality -= 1;
+    } else {
+        item.quality -= 2;
     }
 
+    item.quality = max(item.quality, 0);
     item.sell_in -= 1;
+}
 
-    if item.sell_in < 0 {
-        if item.quality < 50 {
-            item.quality += 1;
-        }
+pub fn update_aged_brie(item: &mut Item) {
+    if item.sell_in > 0 {
+        item.quality += 1;
+    } else {
+        item.quality += 2;
     }
+
+    item.quality = min(item.quality, 50);
+    item.sell_in -= 1;
 }
 
 pub fn update_backstage_passes(item: &mut Item) {
-    if item.quality < 50 {
+    if item.sell_in <= 0 {
+        item.quality = 0;
+    } else if item.sell_in < 6 {
+        item.quality += 3;
+    } else if item.sell_in < 11 {
+        item.quality += 2;
+    } else {
         item.quality += 1;
     }
-    if item.sell_in < 11 {
-        if item.quality < 50 {
-            item.quality += 1;
-        }
-    }
-    if item.sell_in < 6 {
-        if item.quality < 50 {
-            item.quality += 1;
-        }
-    }
 
+    item.quality = min(item.quality, 50);
     item.sell_in -= 1;
-
-    if item.sell_in < 0 {
-        item.quality = 0;
-    }
-}
-
-pub fn update_regular_item(item: &mut Item) {
-    if item.quality > 0 {
-        item.quality -= 1;
-    }
-
-    item.sell_in -= 1;
-
-    if item.sell_in < 0 {
-        item.quality -= 1;
-    }
 }
 
 pub struct GildedRose {
